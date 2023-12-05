47894760be5192e8521c3c135a58afa4859e201f
djchill1 <djchill10@googlemail.com>
2023-12-05 09:28:23 +0000

    day 5 ⭐ attempt at part 2

--- 5/5.py
+++ 5/5.py
@@ -4,7 +4,7 @@ import re
 data = init.read_data(isTest=False, )
 
 
-def math_remap_seed(seeds, rangesList):
+def math_remap_seeds(seeds, rangesList):
 	for index, seed in enumerate(seeds):
 		for row in rangesList:
 			row = init.ints(row)
@@ -14,6 +14,22 @@ def math_remap_seed(seeds, rangesList):
 				seeds[index] = newSeed
 				print('can update!', seed, newSeed)
 	return seeds
+
+
+def math_remap_seed(seed, rangesList):
+	for row in rangesList:
+		row = init.ints(row)
+		# print('checking', row)
+		if seed in range(row[1], row[1]+row[2]):
+			# can update
+			newSeed = (seed - row[1]) + row[0]
+			# print('can update!', seed, newSeed)
+			seed = newSeed
+			return seed
+		else:
+			# print('cannot update', seed)
+			continue
+	return seed
 
 
 def part1():
@@ -34,7 +50,7 @@ def part1():
 
 	# loop through each map group for every seed
 	for mapRange in mapIndexes:
-		seeds = math_remap_seed(seeds, data[mapRange[0]:mapRange[1]])
+		seeds = math_remap_seeds(seeds, data[mapRange[0]:mapRange[1]])
 
 		print(seeds)
 
@@ -42,7 +58,33 @@ def part1():
 
 
 def part2():
-	return False
+	print('part 2')
+	seeds = init.ints(data[0])
+	indexes = []
+	mapIndexes = []
+	for index, line in enumerate(data):
+		if 'map' in line:
+			indexes.append(index)
+	indexes.append(len(data))
+	# print(indexes)
+	for index, entry in enumerate(indexes):
+		try:
+			mapIndexes.append((entry + 1, indexes[index + 1] - 1))
+		except:
+			continue
+	print(mapIndexes)
+	minLocation = 1000000000000000000
+	# loop through each map group for every seed
+	seed_starts = [(seeds[0], seeds[1]), (seeds[2], seeds[3])]
+	for info in seed_starts:
+		for seed in range(info[0], info[0]+info[1]):
+			# print(seed)
+			for mapRange in mapIndexes:
+				seed = math_remap_seed(seed, data[mapRange[0]:mapRange[1]])
+			if seed < minLocation:
+				minLocation = seed
+
+	return minLocation
 
 
 print(f'Part 1: {part1()}, Part 2: {part2()}')
