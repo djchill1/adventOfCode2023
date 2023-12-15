f98cfb418e6d6d7a800074a33eb18ccfad318fd7
djchill1 <djchill10@googlemail.com>
2023-12-15 22:51:28 +0000

    day 15 ⭐⭐

--- 15/15.py
+++ 15/15.py
@@ -25,7 +25,68 @@ def part1():
 
 
 def part2():
-	return False
+	boxes = {}
+
+	for i in range(0,256):
+		boxes[i] = {}
+
+	for step in steps:
+		commands = re.split('', step)
+		commands = commands[1:len(commands)-1]
+		current_value = 0
+		left = ''
+		# current value is box to check.
+		for value in commands:
+			if value.isalpha():
+				left = left+value
+				current_value = hash(value, current_value)
+			elif value in ('=', '-'):
+				rule = value
+			elif value.isnumeric():
+				lens = value
+
+		# print('after', step)
+		if rule == '=':
+
+			# add lens to box number current_value
+			current_box = boxes[current_value]
+			# print(current_box)
+
+			# if already a lens with same label, replace value in situ.
+			try:
+				boxes[current_value][left] = lens
+
+			# else add to end of box.
+			except:
+				boxes[current_value][left] = lens
+		else:
+			# try to remove lens if present & bunch every other lens forward
+			try:
+				boxes[current_value].pop(left)
+			except:
+				pass
+		# print(boxes)
+
+	# calculate power
+	total_focusing_power = 0
+
+	for box_number in boxes:
+		print(box_number, boxes[box_number])
+
+		lens_slot_within_box = 1
+		for lens in boxes[box_number]:
+			lens_value = boxes[box_number][lens]
+
+			focusing_power = (1 + int(box_number)) * lens_slot_within_box * int(lens_value)
+			print('lens at', lens, 'has power', focusing_power)
+			total_focusing_power += focusing_power
+
+			lens_slot_within_box += 1
+
+	return total_focusing_power
 
 
 print(f'Part 1: {part1()}, Part 2: {part2()}')
+
+
+#I had this problem as well — turns out I assumed all label strings were of length 2, which isn't the case in the actual input.
